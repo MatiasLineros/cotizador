8e9ecdbcc6226b9caad9f428d20d046802cf6b46
Matias Lineros R <49159973+MatiasLineros@users.noreply.github.com>
2021-03-15 19:26:06 -0300

    cambio de nombre

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    API COTIZADOR
                 </div>
 
                 <div class="links">
