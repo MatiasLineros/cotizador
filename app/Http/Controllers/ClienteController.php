2bb576393c3ceae7df410c4435bd628330244016
Matias Lineros R <49159973+MatiasLineros@users.noreply.github.com>
2021-03-19 15:38:52 -0300

    clientes index
    ruta y funcion index

--- app/Http/Controllers/ClienteController.php
+++ app/Http/Controllers/ClienteController.php
@@ -3,8 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Cliente;
 
 class ClienteController extends Controller
 {
-    //
+    public function index(){
+    	//$clientes = Cliente::all();
+    	$clientes = Cliente::get(['id', 'rut', 'nombre', 'apellido', 'telefono', 'direccion']);
+
+    	return response()->json(array(
+   			'clientes' => $clientes,
+    		'status' => 'success'
+   		), 200);
+    }
 }
