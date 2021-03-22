d6918f1f3f039797f4105a3631fe41c3674dc549
Matias Lineros R <49159973+MatiasLineros@users.noreply.github.com>
2021-03-22 17:53:15 -0300

    agrupar por tipo

--- app/Http/Controllers/CotizacionMedidaController.php
+++ app/Http/Controllers/CotizacionMedidaController.php
@@ -10,7 +10,7 @@ class CotizacionMedidaController extends Controller
     public function create(){
 		//y mandar solo los q estan vinculado a las cortinas predefinidas
 		$rollers = Roller::join('cortinas_predefinidas', 'cortinas_predefinidas.roller_id', '=', 'rollers.id')
-                         ->groupBy('rollers.id')
+                         ->groupBy('rollers.tipo')
 						 ->get(['rollers.id', 'rollers.tipo']);
 
         return response()->json(array(
