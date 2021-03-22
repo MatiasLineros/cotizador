b33da9e4a1820696b81bd3c0bce97943792ea783
Matias Lineros R <49159973+MatiasLineros@users.noreply.github.com>
2021-03-22 20:59:11 -0300

    roller show y ruta

--- app/Http/Controllers/RollerController.php
+++ app/Http/Controllers/RollerController.php
@@ -3,8 +3,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Roller;
 
 class RollerController extends Controller
 {
-    //
+    public function show($id){
+    	$roller =  Roller::find($id)
+    	if(is_object($roller)){
+    		return response()->json(array(
+	            'roller' => $roller,
+	            'status' => 'success'
+        	), 200);
+    	}else{
+    		return response()->json(array(
+	            'message' => "Roller no existe",
+	            'status' => 'error'
+        	), 300);
+    	}
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -22,5 +22,6 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 
 Route::resource('/usuarios', 'UserController'); // php artisan route:list
 Route::resource('/clientes', 'ClienteController'); // php artisan route:list
+Route::resource('/rollers', 'RollerController'); // php artisan route:list
 Route::resource('/cotizacion-medida', 'CotizacionMedidaController'); // php artisan route:list
 Route::post('/login', 'UserController@login');
