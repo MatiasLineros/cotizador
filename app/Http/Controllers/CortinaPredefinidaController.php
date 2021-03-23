63527f97a2cef2aaefe82f706d23b937a679f063
Matias Lineros R <49159973+MatiasLineros@users.noreply.github.com>
2021-03-23 14:56:26 -0300

    solucion colores y medidas de cortinas predefinidas

--- app/Http/Controllers/CortinaPredefinidaController.php
+++ app/Http/Controllers/CortinaPredefinidaController.php
@@ -9,12 +9,16 @@ class CortinaPredefinidaController extends Controller
 {
     public function cortinasSegunRoller($roller_id){
     	$cortinas = CortinaPredefinida::join('medidas', 'medidas.id', '=', 'cortinas_predefinidas.medida_id')
-    				->join('colores', 'colores.id', '=', 'cortinas_predefinidas.color_id')
     				->where('cortinas_predefinidas.roller_id', $roller_id)->get();
-    				
-    	if(!$cortinas->isEmpty()){
+
+    	$colores = Color::join('colores_rollers_predefinidos', 'colores_rollers_predefinidos.color_id', '=', 'colores.id')->where('colores_rollers_predefinidos.roller_id', $roller_id)
+                         ->groupBy('colores.id', 'colores.color')
+                         ->get(['colores.id', 'colores.color']);
+
+    	if(!$cortinas->isEmpty() && !$colores->isEmpty()){
 	    	return response()->json(array(
 		        'cortinas' => $cortinas,
+		        'colores' => $colores,
 		        'status' => 'success'
 	        ), 200);
 	    }else{
--- app/Http/Controllers/CotizacionMedidaController.php
+++ app/Http/Controllers/CotizacionMedidaController.php
@@ -13,18 +13,12 @@ class CotizacionMedidaController extends Controller
     public function create(){
 		//y mandar solo los q estan vinculado a las cortinas predefinidas
 		$rollers = Roller::join('cortinas_predefinidas', 'cortinas_predefinidas.roller_id', '=', 'rollers.id')
+                         ->join('colores_rollers_predefinidos', 'colores_rollers_predefinidos.roller_id', '=', 'rollers.id')
                          ->groupBy('rollers.id', 'rollers.tipo')
-						 ->get(['rollers.id', 'rollers.tipo']);
-
-        $cortinas_predefinidas = CortinaPredefinida::all();
-        $colores = Color::all();
-        $medidas = Medida::all();
+                         ->get(['rollers.id', 'rollers.tipo']);
 
         return response()->json(array(
             'rollers' => $rollers,
-            'colores' => $colores,
-            'medidas' => $medidas,
-            'cortinas_predefinidas' => $cortinas_predefinidas,
             'status' => 'success'
         ), 200);
         
